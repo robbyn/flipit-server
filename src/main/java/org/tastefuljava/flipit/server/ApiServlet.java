c0c597fb2774067dc8d70cc2eedeb5d355fffc09
Maurice Perry <maurice@perry.productions>
2019-09-19 10:53:58 +0200

    Added datasource

--- src/main/java/org/tastefuljava/flipit/server/ApiServlet.java
+++ src/main/java/org/tastefuljava/flipit/server/ApiServlet.java
@@ -2,11 +2,19 @@ package org.tastefuljava.flipit.server;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import org.tastefuljava.jsonia.JSon;
 
 @WebServlet(name = "ApiServlet", urlPatterns = {"/api/*"})
@@ -14,12 +22,23 @@ public class ApiServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (final PrintWriter out = response.getWriter()) {
-            JSon.write(new Object() {
-                String auth = "oauth";
-                int value = 123;
-            }, out, true);
+        try {
+            Context cxt = new InitialContext();
+            DataSource ds = (DataSource)cxt.lookup("java:/comp/env/jdbc/flipit");
+            try (Connection cnt = ds.getConnection()) {
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(ApiServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            response.setContentType("text/html;charset=UTF-8");
+            try (final PrintWriter out = response.getWriter()) {
+                JSon.write(new Object() {
+                    String auth = "oauth";
+                    int value = 123;
+                }, out, true);
+            }
+        }   catch (NamingException ex) {
+            Logger.getLogger(ApiServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
