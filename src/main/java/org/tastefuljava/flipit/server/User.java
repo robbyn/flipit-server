5490d46faaf96e6311b620faac23d9ef0e3c8b94
maurice <maurice@perry.productions>
2019-09-19 13:33:21 +0200

    domain classes

--- src/main/java/org/tastefuljava/flipit/server/User.java
+++ src/main/java/org/tastefuljava/flipit/server/User.java
@@ -1,5 +1,35 @@
 package org.tastefuljava.flipit.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    
+    private int id;
+    private String email;
+    private String displayName;
+    private final List<Facet> facets = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public List<Facet> getFacets() {
+        return facets;
+    }
 }
