dfbcb0b02a1f6f5b8713dd3385d081780c726abd
maurice <maurice@perry.productions>
2019-12-13 20:49:46 +0100

    change initial password

--- conf/initdb.sql
+++ conf/initdb.sql
@@ -1,2 +1,2 @@
 insert into users(EMAIL, PASSWORD_HASH)
-    values ('maurice@perry.ch','937e8d5fbb48bd4949536cd65b8d35c426b80d2f830c5c308e2cdec422ae2244');
+    values ('maurice@perry.ch','00810cf8b94d6fcb9c5de484d3bec4187620b3e2876e59aab90d852fe0f18fb6');
